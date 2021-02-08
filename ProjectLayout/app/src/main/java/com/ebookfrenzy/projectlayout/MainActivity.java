78e453a269e3fce9c80c3d2c5fb5a2f2172729ed
gthenning4 <ghenning@wccnet.edu>
2021-02-08 16:43:28 -0500

    layout project update

--- ProjectLayout/app/src/main/java/com/ebookfrenzy/projectlayout/MainActivity.java
+++ ProjectLayout/app/src/main/java/com/ebookfrenzy/projectlayout/MainActivity.java
@@ -1,21 +1,14 @@
-package com.ebookfrenzy.statechange;
+package com.ebookfrenzy.projectlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.util.Log;
-import android.widget.EditText;
-
-import androidx.annotation.NonNull;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "StateChange";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Log.i(TAG,"onCreate");
     }
 }
